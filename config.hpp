6bb29c335f3dfecca0ac132555a6b4e98bdb3369
Mehdi <Mehdi0xC@gmail.com>
2019-10-17 17:48:17 +0330

    adding files

---
+++ config.hpp
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+class Config 
+{ 
+    public: 
+    const static int nStates = 7;
+    const static int nActions = 3;
+    const static int nNeurons = 16;
+    constexpr static float gamma = 0.9;
+    constexpr static float learningRate = 0.001;
+    const static int nSamples = 4;
+    const static int memoryCapacity = 1000;
+}; 
+
+#endif
