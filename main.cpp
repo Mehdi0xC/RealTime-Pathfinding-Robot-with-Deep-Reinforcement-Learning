eabae4368386f4f1f0e33eff9ba827234d0108a1
Mehdi <Mehdi0xC@gmail.com>
2019-10-17 17:48:22 +0330

    adding files

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include "config.hpp"
+#include "utils.hpp"
+#include "brain.hpp"
+using namespace std;
+
+
+
+int main()
+{
+    
+    string command;
+    DQN agent;
+    float ** state = Utils::create2DArray(1,Config::nStates);
+    float ** reward = Utils::create2DArray(1, 1);
+    while(true)
+    {
+        cin >> command;
+        Utils::stateDecoder(state, command);
+        cin >> command;
+        Utils::rewarder(reward, command);
+        cout << agent.update(reward, state) << endl;
+    }
+    
+    
+    //    while(true)
+    //    {
+    //     cin >> command;
+    //     if(command == "A")
+    //     {
+    //         cin >> command;
+    //         Utils::stateDecoder(state, command);
+    //         cout << agent.update(reward, state);
+    //     }
+    //
+    //    }
+    
+    
+}
+
