cc596f58f7e6d73dd6388b2fcd0eb02f5465ec18
Mehdi <Mehdi0xC@gmail.com>
2019-10-17 19:33:46 +0330

    update

--- config.hpp
+++ config.hpp
@@ -4,13 +4,13 @@
 class Config 
 { 
     public: 
-    const static int nStates = 7;
+    const static int nStates = 3;
     const static int nActions = 3;
-    const static int nNeurons = 16;
+    const static int nNeurons = 10;
     constexpr static float gamma = 0.9;
     constexpr static float learningRate = 0.001;
     const static int nSamples = 4;
-    const static int memoryCapacity = 1000;
+    const static int memoryCapacity = 10000;
     const static int softmaxTemperature = 10;
     const static int learningIterations = 2500;
 }; 
